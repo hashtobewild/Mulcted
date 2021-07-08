82b4fecb3fbe8b7c762dfc88cae381e50572e2ed
HashToBeWild <loki@swarm.technology>
2021-07-08 22:56:23 +0200

    UI Implemented

--- Mulcted/MainWindow.xaml.cs
+++ Mulcted/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Mulcted.Processes.Implementations;
+using Mulcted.Processes.Interfaces;
+using Mulcted.Processes.Models;
+using System.ComponentModel;
 using System.Windows;
 
 namespace Mulcted
@@ -6,18 +9,123 @@ namespace Mulcted
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private int _age;
+        private TaxPayableItem _currentItem;
+        private decimal _relief;
+        private decimal _salary;
+        private ITaxCalculator _taxCalculator;
+        private decimal _total;
+
         public MainWindow()
         {
+            _taxCalculator = new TaxCalculator();
             InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value != _age)
+                {
+                    _age = value;
+                    OnPropertyChanged("Age");
+                }
+            }
+        }
+
+        public TaxPayableItem CurrentItem
+        {
+            get
+            {
+                return _currentItem;
+            }
+            set
+            {
+                if (value != _currentItem)
+                {
+                    _currentItem = value;
+                    OnPropertyChanged("CurrentItem");
+                }
+            }
+        }
+
+        public decimal Relief
+        {
+            get
+            {
+                return _relief;
+            }
+            set
+            {
+                if (value != _relief)
+                {
+                    _relief = value;
+                    OnPropertyChanged("Relief");
+                }
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value != _salary)
+                {
+                    _salary = value;
+                    OnPropertyChanged("Salary");
+                }
+            }
+        }
+
+        public decimal Total
+        {
+            get
+            {
+                return _total;
+            }
+            set
+            {
+                if (value != _total)
+                {
+                    _total = value;
+                    OnPropertyChanged("Total");
+                }
+            }
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void DecimalUpDown_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Salary = (decimal)e.NewValue;
         }
 
         private void Go_Click(object sender, RoutedEventArgs e)
         {
-            var testingCalc = new TaxCalculator();
-            var xx = testingCalc.Calculate(51, 120000);
-            var yy = xx.DeductionAmount;
+            CurrentItem = _taxCalculator.Calculate(Age, Salary);
+            Relief = CurrentItem.ReliefAmount;
+            Total = CurrentItem.DeductionAmount;
+        }
+
+        private void IntegerUpDown_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Age = (int)e.NewValue;
         }
     }
 }
